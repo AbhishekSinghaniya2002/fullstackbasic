9ef0468f70810a6b07c25e23e72e02763687a5df
Abhishek Singh <abhisheksingh02431@gmail.com>
2024-11-11 08:44:53 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+import { useEffect  } from 'react'
+
+function App(){
+  const [jokes , setJokes]  = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes') // here i have taken a standard approach (/api/jokes)
+    .then((response) =>{
+      setJokes(response.data)
+  })
+
+  .catch((error)=>{
+    console.log(error)
+
+  });
+});
+
+
+  return (
+    <>
+      <h1> Hello how are you</h1>
+      <p>JOKES: {jokes.length}</p>
+      {
+        jokes.map((joke, index) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+
+          </div>
+
+        ))
+      }
+    </>
+  );
+}
+
+
+export default App;
+
+
+
+
